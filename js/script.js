b8bf2fe502c45e593bd7b98ad50cae62459cc236
Ahmadkabiri <Ahmadkabiriw@gmail.com>
2022-12-03 21:58:00 +0330

    wether app project

--- js/script.js
+++ js/script.js
@@ -1,55 +1,61 @@
-﻿let userNameInput = document.querySelector('.username')
-let passwordInput = document.querySelector('.password')
-let modal = document.querySelector('.modal')
+﻿
+let backGroundImage = document.querySelector('.image-container');
+// console.log(backGroundImage);
 
-let usernameMessage = document.querySelector(".username-validation") 
-let passwordMessage = document.querySelector(".password-validation")
+let searchBox = document.querySelector(".input-searchbox");
 
+let searchIcon = document.querySelector(".btn");
 
-function dataValidation() {
-    let userNameValue = userNameInput.value
-    let passwordValue = passwordInput.value
-
-    if (userNameValue.length < 12 || passwordValue.length < 8) {
-        // alert('Error')
-        modal.style.background = 'rgb(223, 28, 28)'
-        modal.innerHTML = 'لطفا اطلاعات را به درستی وارد نمایید'
-        modal.style.display = 'inline'
-    } else {
-        modal.style.background = 'green'
-        modal.innerHTML = 'لاگین با موفقیت انجام شد'
-        modal.style.display = 'inline'
-    }
-
-    setTimeout(function () {
-        modal.style.display = 'none'
-    }, 3000)
-}
+let underSearchBox = document.querySelector(".under-Searchbox");
 
 
-function usernameValidation () {
-    console.log(userNameInput.value.input)
-    if(userNameInput.value.length < 12){
-        usernameMessage.style.display = "block"
-        usernameMessage.style.color = "red" ;
-        usernameMessage.innerHTML = "از 12 کاراکتر کمتر است"
-    }else{
-        usernameMessage.style.display = "block"
-        usernameMessage.style.color = "green" ;
-        usernameMessage.innerHTML = "حالا اوکی شد"
-    }
-}
+(function () {
+    let number = Math.floor(Math.random()*34)+1 ;
+    backGroundImage.style.backgroundImage = `url(/images/${number}.jpg)`
+})()
 
-function passwordValidation () {
-    if(passwordInput.value.length < 8){
-        passwordMessage.style.display = "block"
-        passwordMessage.style.color = "red" ;
-        passwordMessage.innerHTML = "از 8 کاراکتر کمتر است"
-    }else{
-        passwordMessage.style.display = "block"
-        passwordMessage.style.color = "green" ;
-        passwordMessage.innerHTML = "حالا اوکی شد"
-    }
-}
-;
+
+
+const weatherDatabase = [
+    { city : "Tehran" , tempreture : "9°C" , Humidity : '15%' , Weather : "Windy",  Windspeed : '7 km/h'} ,
+    { city : "Esfahan" , tempreture : "12°C" , Humidity : '10%' , Weather : "Windy" , Windspeed : '15 km/h'} ,
+    { city : "Shiraz" , tempreture : "15°C" , Humidity : '12%' , Weather : "Windy" , Windspeed : '5 km/h'} ,
+    { city : "Tabriz" , tempreture : "20°C" , Humidity : '25%' , Weather : "Cloudy" , Windspeed : '20 km/h'} ,
+    { city : "Zanjan" , tempreture : "4°C" , Humidity : '10%' , Weather : "Snowy" , Windspeed : '11 km/h'} ,
+    { city : "Qazvin" , tempreture : "15°C" , Humidity : '8%' , Weather : "foggy" , Windspeed : '25 km/h'} ,
+    { city : "Kish" , tempreture : "28°C" , Humidity : '35%' , Weather : "Rainy", Windspeed : '15 km/h'} ,
+    { city : "Mashhad" , tempreture : "30°C" , Humidity : '14%' , Weather : "calm", Windspeed : '6 km/h'} ,
+]
+
+searchIcon.addEventListener("click",()=>{
+    let data = searchBox.value ;
+    let selectedCity = weatherDatabase.find((item)=>{
+        return item.city===data
+    })
+    let htmlLines = ''
+    htmlLines += `<div class="location">
+    <p> Weather In ${selectedCity.city}</p>
+</div>
+<div class="degree">
+    <p> ${selectedCity.tempreture}</p>
+</div>
+<div class="weather">
+    <p> <span> <i class="fas fa-cloud-moon"></i> </span> ${selectedCity.Weather}</p>
+</div>
+<div class="details">
+    <p>Humidity : ${selectedCity.Humidity} </p>
+    <p>Wind speed : ${selectedCity.Windspeed}/h</p>
+</div>` ;
+
+underSearchBox.innerHTML = htmlLines ;
+searchBox.value = '' ;
+
+
+
+})
+
+
+
+
+
 
