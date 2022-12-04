7f5d0b8a718c82790cd148a6028e865df67e7712
Ahmadkabiri <Ahmadkabiriw@gmail.com>
2022-12-04 20:26:44 +0330

    Celcius to fahrenheit converter

--- js/script.js
+++ js/script.js
@@ -1,16 +1,57 @@
 ﻿
+let btns = document.querySelectorAll(".btn");
 
-let input = document.querySelector(".input");
+let input = document.querySelector("input")
 
+let h1Elem = document.querySelector("h1")
 
-input.addEventListener("keyup",()=>{
-    console.log("Up")
+let pElem = document.querySelector("p")
+
+let h1Flag = true ;
+
+btns[2].addEventListener("click", ()=>{
+    
+    if(h1Flag){
+
+        input.placeholder = "°C"
+        h1Elem.innerHTML = "Converter °C to °F"
+        input.value = '' ;
+        h1Flag = !h1Flag
+    }else{
+        input.placeholder = "°F"
+        h1Elem.innerHTML = "Converter °F to °C"
+        input.value = '' ;
+        h1Flag = !h1Flag
+    }
 })
 
-input.addEventListener("keypress",()=>{
-    console.log("press")
+btns[0].addEventListener("click",()=>{
+
+
+    if(!isNaN(input.value) && input.value !== ""){
+        if(!h1Flag){
+            let celcius = input.value ;
+            let fahrenheit = ((celcius*3.6)+32).toFixed(3) ;
+            pElem.style.color = "rgb(70, 235, 128)"
+            pElem.innerHTML = `${celcius}°C to ${fahrenheit}°F`;
+        }else{
+           let fahrenheit = input.value ;
+            let celcius = ((fahrenheit-32)/1.8).toFixed(3) ;
+            pElem.style.color = "rgb(70, 235, 128)"
+            pElem.innerHTML = `${fahrenheit}°F to ${celcius}°C`;
+        }
+    }else if (input.value === ""){
+        pElem.style.color= "rgb(169, 26, 26)"
+        pElem.innerHTML = `Input is Empty`
+    }else{
+        pElem.innerHTML = `Wrong Input Entered`
+
+    }
+
 })
 
-input.addEventListener("keydown",()=>{
-    console.log("down")
+btns[1].addEventListener("click",()=>{
+    pElem.innerHTML = "" ;
+    input.value = '' ;
+
 })
