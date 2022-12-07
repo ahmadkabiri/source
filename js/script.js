a3a38ad4dda8c5db992e20765059c93f6163f7c9
Ahmadkabiri <Ahmadkabiriw@gmail.com>
2022-12-07 16:46:59 +0330

    remove elements from DOM in JS

--- js/script.js
+++ js/script.js
@@ -1,57 +1,13 @@
 ﻿
-let btns = document.querySelectorAll(".btn");
+const liItems = document.getElementsByClassName("list-item");
 
-let input = document.querySelector("input")
-
-let h1Elem = document.querySelector("h1")
-
-let pElem = document.querySelector("p")
-
-let h1Flag = true ;
-
-btns[2].addEventListener("click", ()=>{
-    
-    if(h1Flag){
-
-        input.placeholder = "°C"
-        h1Elem.innerHTML = "Converter °C to °F"
-        input.value = '' ;
-        h1Flag = !h1Flag
-    }else{
-        input.placeholder = "°F"
-        h1Elem.innerHTML = "Converter °F to °C"
-        input.value = '' ;
-        h1Flag = !h1Flag
-    }
+let liItemsArray = [...liItems]
+liItemsArray.forEach((li)=>{
+    li.addEventListener("click",(event)=>{
+        console.log(event.target.parentElement.remove())
+    })
 })
 
-btns[0].addEventListener("click",()=>{
 
 
-    if(!isNaN(input.value) && input.value !== ""){
-        if(!h1Flag){
-            let celcius = input.value ;
-            let fahrenheit = ((celcius*3.6)+32).toFixed(3) ;
-            pElem.style.color = "rgb(70, 235, 128)"
-            pElem.innerHTML = `${celcius}°C to ${fahrenheit}°F`;
-        }else{
-           let fahrenheit = input.value ;
-            let celcius = ((fahrenheit-32)/1.8).toFixed(3) ;
-            pElem.style.color = "rgb(70, 235, 128)"
-            pElem.innerHTML = `${fahrenheit}°F to ${celcius}°C`;
-        }
-    }else if (input.value === ""){
-        pElem.style.color= "rgb(169, 26, 26)"
-        pElem.innerHTML = `Input is Empty`
-    }else{
-        pElem.innerHTML = `Wrong Input Entered`
 
-    }
-
-})
-
-btns[1].addEventListener("click",()=>{
-    pElem.innerHTML = "" ;
-    input.value = '' ;
-
-})
