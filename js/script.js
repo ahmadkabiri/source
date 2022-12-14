50489920ab0864621674581d79a1bb33d068a4f3
AhmadKabiri <51478462+ahmadkabiri@users.noreply.github.com>
2022-12-14 11:53:24 +0330

    note App project(dirty code)

--- js/script.js
+++ js/script.js
@@ -1,33 +1,119 @@
 ﻿
-let container = document.querySelector(".container")
+const $ = document ;
+
+let inputElem = $.querySelector("input") ;
+
+let btnElem = [...$.querySelectorAll(".btn")] ;
+
+console.log(btnElem)
+
+let btnsElem = [...$.querySelectorAll(".btns")] ;
+
+let container = $.querySelector(".container")
 
 
-function contextMenuHandler (event) {
-    event.preventDefault() 
-    
-    console.log(event)
-    
-    let pageX = event.pageX ;
-    let pageY = event.pageY ;
-    
-    container.style.display = 'block' ;
-    container.style.left = `${pageX}px` ;
-    container.style.top = `${pageY}px` ;
+// function makeNote (event) {
+// console.log(event) ;
+
+// if(event.keyCode === 13 && inputElem.value!==""){
+
+//     let taskWords = inputElem.value ;
+
+//     let div = $.createElement("div") ;
+//     console.log(div)
+
+//     div.className = "task" ;
+//     let p = $.createElement("p") ;
+
+//     // div.style.backgroundColor= inputColor ;
+
+//     p.innerHTML = taskWords ;
+
+//     div.append(p) ;
+//     container.append(div) ;
+
+//     inputElem.value = ""; 
+//     inputElem.style.backgroundColor = "#f2f3f5" ;
+// }else{
+// //     inputElem.style.backgroundColor = "#f2f3f5" ;
+
+// }
+// }
+
+
+
+
+// inputElem.addEventListener("keydown", makeNote) ;
+
+
+let inputColor ;
+
+btnElem.forEach(el=>{
+    el.addEventListener("click",(event)=>{
+        inputColor = event.target.classList[1]
+        console.log(inputColor) ;
+        inputElem.style.backgroundColor = inputColor ;
+        inputElem.addEventListener("keydown", (event)=>{
+            console.log(event) ;
+
+if(event.keyCode === 13 && inputElem.value!==""){
+
+    let taskWords = inputElem.value ;
+
+    let div = $.createElement("div") ;
+    console.log(div)
+
+    div.className = "task" ;
+    let p = $.createElement("p") ;
+
+    div.style.backgroundColor = inputColor ;
+    // div.style.backgroundColor= inputColor ;
+
+    p.innerHTML = taskWords ;
+
+    div.append(p) ;
+    container.append(div) ;
+
+    inputElem.value = ""; 
+    inputElem.style.backgroundColor = "#f2f3f5" ;
+
+    p.addEventListener("click",event=>{
+        event.target.parentElement.remove()
+    })
+}else{
+//     inputElem.style.backgroundColor = "#f2f3f5" ;
 
 }
+        }) ;
+        btnsElem[1].addEventListener("click",event=>{
+            if(inputElem.value!==""){
+                let taskWords = inputElem.value ;
+
+                let div = $.createElement("div") ;
+                console.log(div)
+            
+                div.className = "task" ;
+                let p = $.createElement("p") ;
+            
+                div.style.backgroundColor = inputColor ;
+                // div.style.backgroundColor= inputColor ;
+            
+                p.innerHTML = taskWords ;
+            
+                div.append(p) ;
+                container.append(div) ;
+            
+                inputElem.value = ""; 
+                inputElem.style.backgroundColor = "#f2f3f5" ;
+            }
+        })
+    })
+})
 
 
-function clickHandler (event) {
-    container.style.display = 'none' ;
-    
-}
+console.log(btnsElem)
 
-function keydownHandler (event) {
-    console.log(event)
-     if(event.code==="Escape"){
-    container.style.display = 'none' ;
-     }
-}
-
-
-
+btnsElem[0].addEventListener("click",event=>{
+    inputElem.value = "" ;
+    inputElem.style.backgroundColor = "#fff"
+})
