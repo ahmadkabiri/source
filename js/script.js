3e3d4cfb6a4947c7e850d038ba89bc6bbbd26f46
AhmadKabiri <51478462+ahmadkabiri@users.noreply.github.com>
2022-12-14 16:34:44 +0330

    dataset codes

--- js/script.js
+++ js/script.js
@@ -1,30 +1,15 @@
 ﻿
-const mainNav = document.getElementById("mainNav")
-
-const logoImg = document.querySelector("img")
-
-const btnElem = document.querySelector(".btn")
+const btn = document.querySelectorAll("button")
 
 
 
-document.addEventListener("scroll",()=>{
-    if(document.documentElement.scrollTop > 0){
-        mainNav.classList.add("bg-black") ;
-        mainNav.classList.add("txt-white") ;
-        logoImg.style.height = "64px"
-    }else{
-        mainNav.classList.remove("bg-black") ;
-        mainNav.classList.remove("txt-white") ;
-        logoImg.style.height = "84px"
-    }
+function showDataId (event) {
+    console.log(event.target.dataset.name + " is " + event.target.dataset.id )
+}
+
+
+
+
+btn.forEach(el=>{
+    el.addEventListener("click",showDataId) ;
 })
-
-
-btnElem.addEventListener("click",()=>{
-    window.scrollTo(0,0)
-})
-
-
-// btnElem.addEventListener("click",()=>{
-//     window.scrollBy(0,100)
-// })
