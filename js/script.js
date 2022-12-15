ccd907801a05aa0904089c9ff9d3dd57e631895e
AhmadKabiri <51478462+ahmadkabiri@users.noreply.github.com>
2022-12-15 17:18:44 +0330

    music player simple

--- js/script.js
+++ js/script.js
@@ -1,81 +1,42 @@
-﻿
+﻿const $ = document ;
 
-let audioElem = document.querySelector("audio") ;
+const players = [...$.querySelectorAll(".fa-play")] ;
+const audios = [...$.querySelectorAll("audio")] ;
 
+let musicName ;
 
+players.forEach((player)=>{
+    console.log(player) ;
+    player.addEventListener("click",(event)=>{
+        musicName = event.target.dataset.name ;
+        audios.forEach((audio)=>{
+            if(audio.dataset.name===musicName){
+                audio.currentTime=0
+                audio.play()
+            }else{
+                audio.pause()
+            }
+        })
 
-
-let musicsSrc = [
-    "music/Dadashi.mp3" ,
-    "music/Ey Sareban.mp3" ,
-    "music/mo.mp3" ,
-    "music/Sharare.mp3" ,
-]
-
-let audioIndex = 0 ;
-
-
-function previousMusicHandler () {
-    audioIndex-- ;
-    
-    if( audioIndex < 0 ){
-        audioIndex = 3 ;
-    }
-
-    
-    audioElem.setAttribute("src", musicsSrc[audioIndex])
-    playMusicHandler()
-    console.log(musicsSrc[audioIndex])
-
-}
-
-function playMusicHandler () {
-    audioElem.play() ;
-    console.log("Music played")
-
-    setInterval(()=>{
-        console.log(Math.floor(audioElem.currentTime))
-    },1000)
-
-}
-
-function pauseMusicHandler () {
-    audioElem.pause()
-    console.log("Music paused")
-
-}
-
-function nextMusicHandler () {
-    audioIndex++ ;
-    
-    if( audioIndex > musicsSrc.length-1 ){
-        audioIndex = 0 ;
-    }
-
-    
-    audioElem.setAttribute("src", musicsSrc[audioIndex])
-    playMusicHandler()
-    console.log(musicsSrc[audioIndex])
-
-
-}
+    })
+})
 
 
 
-function musicSpeedHandler () {
-    audioElem.playbackRate = 2 ;
-    console.log("Speed 2X") ;
-}
-
-function timePlusMusicHandler () {
-    audioElem.currentTime += 5 ;
-}
 
 
-function timeMinusMusicHandler () {
-    audioElem.currentTime -= 5 ;
 
-}
+
+
+
+
+
+
+
+
+
+
+
 
 
 
