64e33a773dc0f9c05afaaa9fd680b0732cfee73b
AhmadKabiri <51478462+ahmadkabiri@users.noreply.github.com>
2022-12-21 16:33:46 +0330

    photo-slider-project

--- js/script.js
+++ js/script.js
@@ -1,55 +1,46 @@
 ﻿
+const $ = document ;
 
-let addAnimationBtn = document.querySelector("button")
-let divElem = document.querySelector("div")
-let pElem = document.querySelector("p")
+let prevBtn = $.querySelector(".prev") ; 
+
+let nextBtn = $.querySelector(".next") ; 
+
+let imageElem = $.querySelector(".image")
 
 
 
+let imagesSrc = [
+    {src: "/images/34.jpg"},
+    {src: "/images/35.jpg"},
+    {src: "/images/37.jpg"}
+]
+
+let index = 0 ;
+
+function prevoiusImageHandler () {
+
+    index-- ;
+    // console.log(imagesSrc[index].src)
+    if(index<0){
+        index=imagesSrc.length-1
+    }
+    imageElem.style.backgroundImage = `url(${imagesSrc[index].src})`
 
 
-
-function setAnimation () {
-    // console.log(divElem)
-divElem.style.animation = "move 1s 3"
 }
 
-function animationStartHandler () {
-    console.log("Aniamtion Start")
-    pElem.innerHTML = "Aniamtion Start" ;
-    
-}
+function nextImageHandler () {
+    index++ ;
+    if(index>imagesSrc.length-1){
+        index = 0 ;
+    }
+    imageElem.style.backgroundImage = `url(${imagesSrc[index].src})`
 
-function animationrepeatHandler () {
-    console.log("Aniamtion repeat")
-    pElem.innerHTML = "Aniamtion repeat" ;
-    
-}
-
-function animationEndHandler () {
-    console.log("Aniamtion End")
-    pElem.innerHTML = "Aniamtion End" ;
-    
 }
 
 
+setInterval(nextImageHandler,4000)
 
-addAnimationBtn.addEventListener("click",setAnimation)
+prevBtn.addEventListener('click',prevoiusImageHandler) ;
 
-
-divElem.addEventListener("animationstart",animationStartHandler)
-divElem.addEventListener("animationiteration",animationrepeatHandler)
-divElem.addEventListener("animationend",animationEndHandler)
-
-
-
-
-
-
-
-
-
-
-
-
-
+nextBtn.addEventListener('click',nextImageHandler) ;
