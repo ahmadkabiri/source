7cdec63f4d2d305189535a980896175d987f57ad
AhmadKabiri <51478462+ahmadkabiri@users.noreply.github.com>
2023-01-03 10:39:12 +0330

    shoping structure best project

--- js/script.js
+++ js/script.js
@@ -1,94 +1,173 @@
-﻿
-const list_items = [
-  { id:1 , name: "Amin" , family:"Saeedi Rad"},
-  { id:2 , name: "Amin" , family:"Darabi"},
-  { id:3 , name: "Ali" , family:"Sangi"},
-  { id:4 , name: "Hossain" , family:"Fallah"},
-  { id:5 , name: "Abbas" , family:"Mohammadi"},
-
-  { id:6 , name: "Saheb" , family:"Mohammadi"},
-  { id:7 , name: "Ziba" , family:"Mahmoodi"},
-  { id:8 , name: "Mohammad" , family:"Rajabi"},
-  { id:9 , name: "Fateme" , family:"Rastan"},
-  { id:10 , name: "Sohrab" , family:"Rostami"},
-
-  { id:11 , name: "Ali" , family:"Nazari"},
-  { id:12 , name: "Ramin" , family:"Mohammadian"},
-  { id:13 , name: "Sara" , family:"Zahraie"},
-  { id:14 , name: "Sima" , family:"Araste"},
-  { id:15 , name: "Mahsa" , family:"Amini"},
-
-  { id:16 , name: "Sardar" , family:"Azmoon"},
-  { id:17 , name: "Ali" , family:"Zand Vakili"},
-  { id:18 , name: "Rahman" , family:"Ahmadi"},
-  { id:19 , name: "Mahdi" , family:"Mokhtari"},
-  { id:20 , name: "Hamed" , family:"Ghorbani"},
-
-  { id:21 , name: "Pouria" , family:"Gilani"},
-  { id:22 , name: "hossein" , family:"Bahrami"},
+﻿let allProducts = [
+    {id : 1 , title : "Album 1" , price: 5 , img:"Images/Album 1.png" , count : 1 },
+    {id : 2 , title : "Album 2" , price: 15 , img:"Images/Album 2.png" , count : 1 },
+    {id : 3 , title : "Album 3" , price: 20 , img:"Images/Album 3.png" , count : 1 },
+    {id : 4 , title : "Album 4" , price: 100 , img:"Images/Album 4.png" , count : 1},
+    {id : 5 , title : "Coffee" , price: 5 , img:"Images/Cofee.png" , count : 1},
+    {id : 6 , title : "Shirt" , price: 50 , img:"Images/Shirt.png" , count : 1},
 ]
 
+let userBasket = [] ;
 
-const list_element = document.getElementById("list") ;
 
-const pagination_element = document.getElementById("pagination") ;
 
-let current_page = 1 ;
-let rows = 3 ;
-let pages ;
+let $ = document ;
 
-function displayPagination () {
+const shopItemContainer = $.querySelector(".shop-items")
 
-  pages = Math.ceil(list_items.length/rows)
-  makeButton(pages)
-} ;
+const basketProductsContainer = $.querySelector(".cart-items")
 
-displayPagination()
+const removeAllProductsBtn = $.getElementById("remove-all-products")
 
-function makeButton (pages) {
-console.log(pages)
+const cartTotalPriceElem = $.querySelector(".cart-total-price")
 
-let btnElemArray = []
 
-for(let i=1 ; i<=pages ;i++){
-  let buttonElem = document.createElement("button");
-  buttonElem.innerHTML = i ;
-  buttonElem.setAttribute("id",i)
-  btnElemArray.push(buttonElem)
-  sliceArray(1)
-  buttonElem.addEventListener("click",(event)=>{
-    event.target.classList.add("active") ;
-    let elemId = event.target.id
-    sliceArray(elemId)
-    btnElemArray.forEach(el=>{
-      if(elemId!==el.id){
-        el.classList.remove("active")
-      }
+allProducts.forEach((product)=>{
+    let productContainer = $.createElement("div") ;
+    productContainer.classList.add("shop-item") ;
+
+    let productTitleSpan = $.createElement("span") ;
+    productTitleSpan.classList.add("shop-item-title") ;
+    productTitleSpan.innerHTML = product.title ;
+
+    let productImageElem = $.createElement("img") ;
+    productImageElem.setAttribute("src" , product.img) ;
+    productImageElem.classList.add("shop-item-image") ;
+
+    let productDetailsContainer = $.createElement("div") ;
+    productDetailsContainer.classList.add("shop-item-details") ;
+
+    let productPriceSpan = $.createElement("span") ;
+    productPriceSpan.innerHTML = product.price ;
+    productPriceSpan.classList.add("shop-item-price,") ;
+
+    let productAddButton = $.createElement("button") ;
+    productAddButton.innerHTML = "ADD TO CART" ;
+    productAddButton.className = "btn btn-primary shop-item-button"
+    productAddButton.addEventListener("click",()=>{
+        let countOrAddToProduct = userBasket.some(basketProduct=>{
+            return basketProduct.id === product.id
+        })
+        console.log(countOrAddToProduct)
+        if(!countOrAddToProduct){
+            addProductToBasketArray(product.id)
+        }else{
+            updateProductCount(product.id,product.count+1)
+
+        }
     })
-  })
-  pagination_element.appendChild(buttonElem) ;
+
+    productDetailsContainer.append(productPriceSpan,productAddButton) ;
+    productContainer.append(productTitleSpan,productImageElem,productDetailsContainer)
+
+
+    shopItemContainer.append(productContainer) ;
+})
+
+function addProductToBasketArray (productId) {
+    
+    let mainProduct = allProducts.find((product)=>{
+        return product.id === productId
+    })
+    
+
+    userBasket.push(mainProduct);
+
+    basketProductsGenerator(userBasket)
+    calcTotalPrice(userBasket);
 }
+
+function basketProductsGenerator (userBasketArray) {
+    basketProductsContainer.innerHTML= ''
+    userBasketArray.forEach((product)=>{
+    
+        let basketProductContainer = $.createElement("div") ;
+        basketProductContainer.classList.add("cart-row") ;
+
+        let basketProductDetailsContainer = $.createElement("div") ;
+        basketProductDetailsContainer.className = "cart-item cart-column" ;
+
+        let baskProductImg = $.createElement("img") ;
+        baskProductImg.setAttribute("src",product.img) ;
+        baskProductImg.setAttribute("width","100") ;
+        baskProductImg.setAttribute("height","100") ;
+        baskProductImg.classList.add("cart-item-image") ;
+
+        let basketProductTitleSpan = $.createElement("span") ;
+        basketProductTitleSpan.classList.add("cart-item-title") ;
+        basketProductTitleSpan.innerHTML = product.title ;
+
+        basketProductDetailsContainer.append(baskProductImg, basketProductTitleSpan)
+
+        let basketProductPriceSpan = $.createElement("span") ;
+        basketProductPriceSpan.className = "cart-price cart-column" ;
+        basketProductPriceSpan.innerHTML = product.price ;
+
+        let basketProductInputsContainer = $.createElement("div") ;
+        basketProductInputsContainer.className = "cart-quantity cart-column";
+
+        let basketProductInput = $.createElement("input") ;
+        basketProductInput.className = "cart-quantity-input" ;
+        basketProductInput.value = product.count ;
+        basketProductInput.setAttribute("type","number") ;
+        basketProductInput.setAttribute("min","0") ;
+        basketProductInput.addEventListener("change",()=>{
+            updateProductCount(product.id,basketProductInput.value)
+        })
+
+        let basketProductRemoveBtn = $.createElement("button") ;
+        basketProductRemoveBtn.className = "btn btn-danger" ;
+        basketProductRemoveBtn.innerHTML = "REMOVE" ;
+        basketProductRemoveBtn.addEventListener("click",()=>{
+            removeProductFromBasket(product.id)
+        })
+
+        basketProductInputsContainer.append(basketProductInput, basketProductRemoveBtn)
+
+        basketProductContainer.append(basketProductDetailsContainer,basketProductPriceSpan,basketProductInputsContainer)
+
+        basketProductsContainer.append(basketProductContainer)
+    })
 }
 
-function sliceArray (id) {
-  let idRow = id*rows ;
-  let current_row = list_items.slice(idRow-(rows),idRow)
-  makeRows(current_row) ;
 
-}
+function removeProductFromBasket (productId) {
 
-function makeRows (row) {
-  list_element.innerHTML="";
-  row.forEach((el)=>{
-    let itemDiv = document.createElement("div") ;
-    itemDiv.classList.add("item") ;
-    itemDiv.innerHTML = `${el.name} ${el.family}`
-    list_element.append(itemDiv) ;
-  })
+    userBasket =  userBasket.filter((product)=>{
+        return product.id!== productId
+    })
+    basketProductsGenerator(userBasket) ;
+    updateProductCount(productId,0)
+    console.log(userBasket)
 }
 
 
+removeAllProductsBtn.addEventListener("click",()=>{
+    userBasket = [] ;
+    basketProductsGenerator(userBasket) ;
+})
 
+
+function calcTotalPrice (userBasketArray) {
+    let totalPriceValue = 0 ;
+    totalPriceValue = userBasketArray.reduce((acc,curr)=>{
+        return acc + curr.count*curr.price ;
+    },0)
+    
+    cartTotalPriceElem.innerHTML = totalPriceValue ;
+}
+
+
+function updateProductCount (productId,newCount) {
+    console.log(`product ID : ${productId} new count : ${newCount}`) ;
+    userBasket.forEach(product=>{
+        if(product.id===productId){
+            product.count = newCount ;
+        }
+    })
+
+    calcTotalPrice(userBasket)
+}
 
 
 
