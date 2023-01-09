9ccd097bddf7f1cead5e81f838ef79946a652715
AhmadKabiri <51478462+ahmadkabiri@users.noreply.github.com>
2023-01-09 20:15:44 +0330

    event capturing

--- js/script.js
+++ js/script.js
@@ -6,9 +6,9 @@ console.table(divElems)
 divElems.forEach((div , index)=>{
     div.addEventListener("click",(event)=>{
         console.log("Div"+(index+1))
-        // div.style.backgroundColor="red" => This coding method causes bubbling
-        event.target.style.backgroundColor = "red" // => This coding method prevents bubbling
-    })
+         div.style.backgroundColor="red"  // => This coding method causes bubbling
+        //event.target.style.backgroundColor = "red" // => This coding method prevents bubbling
+    },{capture:true})
 })
 
 
