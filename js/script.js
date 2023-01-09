fa2491a9764b071d1afc1076060273edb2def848
AhmadKabiri <51478462+ahmadkabiri@users.noreply.github.com>
2023-01-09 22:25:33 +0330

    capture usage

--- js/script.js
+++ js/script.js
@@ -1,19 +1,29 @@
 
-const divElems = document.querySelectorAll("div")
+const listItems = document.querySelectorAll('li') ;
 
-console.table(divElems)
+const inputElem = document.querySelector("input")
 
-divElems.forEach((div , index)=>{
-    div.addEventListener("click",(event)=>{
-        console.log("Div"+(index+1))
-         div.style.backgroundColor="red"  // => This coding method causes bubbling
-        //event.target.style.backgroundColor = "red" // => This coding method prevents bubbling
-    },{capture:true})
+const languageContainer = document.querySelector("ul")
+
+
+
+languageContainer.addEventListener("click",(event)=>{
+    console.log(event.target)
+    if(event.target.tagName ==="LI"){
+        event.target.remove()
+    }
 })
 
 
-
-
+inputElem.addEventListener("keypress",(event)=>{
+    if(event.keyCode==13){
+        console.log("enter pressed")
+        let newLanguageLi = document.createElement("li")
+        newLanguageLi.innerHTML = event.target.value ;
+        languageContainer.append(newLanguageLi)
+        inputElem.value='';
+    }
+})
 
 
 
