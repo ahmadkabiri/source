fe7db6339b1dba822b4f790948e7f9c4bf3fb194
AhmadKabiri <51478462+ahmadkabiri@users.noreply.github.com>
2023-01-13 14:27:53 +0330

    callback function

--- js/script.js
+++ js/script.js
@@ -1,45 +1,33 @@
 
-
-let inputElem = document.querySelector(".input");
-let alarmElem = document.querySelector(".alarm");
-let todosUl = document.querySelector(".list");
+const btn = document.querySelector("button")
 
 
-function addTodo (event) {
-    if(!inputElem.value){
-        alarmElem.style.opacity= "1"
-    }else{
-        alarmElem.style.opacity= "0"
-        // console.log(event)
-        if(event.key==="Enter"){
-            // console.log("ok")
-            
-            let newTodo = inputElem.value ;
-            let newLiTag = document.createElement('li') ;
-            // console.log(newLiTag);
-            newLiTag.className = "list-item";
-            let newSpanTag = document.createElement("span") ;
-            newSpanTag.innerHTML =  newTodo.trim() ;
-            let newITag = document.createElement("i");
-            // newITag.addEventListener("click",(event)=>{
-            //     event.target.parentElement.remove()
-            // })
-            // یک نکته که شدیدا باید بهش توجه کنم این هست که وقتی یک آیتم جدید درست میشه و اگر میدونیم قراره یه ایونت بگیره بایستی بعد از ساخته شدنش در خط بعدی این ایونت رو بدیم بهش این مساله خیلی خیلی مهمه
-            newITag.className = 'fa fa-trash' ;
-            newLiTag.append(newSpanTag,newITag) ;
-            console.log(newLiTag) ;
-            todosUl.append(newLiTag)
-            
-            inputElem.value = "" ;
+// function clickHandler () {
+//     console.log("clicked")
+// }
 
-        }
-    }
+// btn.addEventListener("click",clickHandler)
+
+function logger (func) {
+    func()
 }
-// REFACTOR CODES :)))))))
-todosUl.addEventListener("click",event=>{
-    if(event.target.tagName==="LI")
-    event.target.remove()
+
+
+function logger2 () {
+    console.log("Logged 2")
+}
+
+
+
+logger(logger2)
+
+let numbers = [1,2,3,4,5]
+
+numbers.map(item=>{
+    console.log(item)
 })
 
-inputElem.addEventListener("keydown",addTodo)
+
+
+
 
