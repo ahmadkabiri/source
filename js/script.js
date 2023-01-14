2241cd3a10eb71a2ed6db315d7231a532d455070
AhmadKabiri <51478462+ahmadkabiri@users.noreply.github.com>
2023-01-14 13:37:29 +0330

    arguments

--- js/script.js
+++ js/script.js
@@ -1,34 +1,19 @@
 "use strict"
 
-//  function sum(num1,num2) {
-//     let addNumbers = num1 + num2 
-//     return addNumbers
-//  }
+
+function sum() {
+    let toArray = Array.from(arguments)
+    let total = toArray.reduce((acc,curr)=>{
+        return acc+curr
+    },0)
+    return total
+}
+
+                       
 
 
-// const sum = (num1,num2)=>  {
-//     let addNumbers = num1 + num2 
-//     return addNumbers
-//  }
+ const sumNumbers = sum(1,2,3,4,5)
 
-// const pow = (num1,num2)=>{
-//     return num1**num2
-// }
-
-// const pow = (num1,num2)=> num1**num2
-
-
-// const logger = val => console.log(val) ;
-
-// const generateUser = (id,name,age=12) => ({id,name,age}) 
-
-
-
-
-// let ali = generateUser(1,"ali")
-
-// console.log(ali)
-
-
+ console.log(sumNumbers)
 
 
