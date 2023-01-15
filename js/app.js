1ec656e210dee8668e5f0fd583db5f2d1f291131
AhmadKabiri <51478462+ahmadkabiri@users.noreply.github.com>
2023-01-16 00:25:50 +0330

    email validate with regEx

---
+++ js/app.js
@@ -0,0 +1,20 @@
+
+let emailRegEx = /.+@.+.com/ 
+
+let userEmial = prompt("please Enter Your Email")
+
+console.log(emailRegEx.test(userEmial))
+
+// amin@gmail.com babk@gmail.com anir_amiri@gmail.com 
+
+
+
+
+
+
+
+
+
+
+
+
