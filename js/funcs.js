71dc03c2608593c2b396937647f8eb275975529a
AhmadKabiri <51478462+ahmadkabiri@users.noreply.github.com>
2023-01-15 15:04:59 +0330

    module ?

--- js/funcs.js
+++ js/funcs.js
@@ -1,27 +1,77 @@
 let users = [
-    {id:18 , name:"ali" ,age:22} ,
-    {id:19 , name:"amin" ,age:23} ,
-    {id:29 , name:"amir" ,age:19} ,
-    {id:36 , name:"sasan" ,age:28} ,
-    {id:41 , name:"qadir" ,age:20} ,
+    {id:10 , name:"ali"   ,age:22} ,
+    {id:11 , name:"amin"  ,age:23} ,
+    {id:20 , name:"amir"  ,age:19} ,
+    {id:22 , name:"sasan" ,age:28} ,
+    {id:30 , name:"qadir" ,age:20} ,
+    {id:33 , name:"ahmad" ,age:34} ,
+    {id:40 , name:"reza"  ,age:29} ,
+    {id:44 , name:"shima" ,age:39} ,
+    {id:50 , name:"sahar" ,age:16} ,
+    {id:55 , name:"samira",age:24} ,
+    {id:60 , name:"sadra" ,age:25} ,
+    {id:66 , name:"arman" ,age:19} ,
+    {id:70 , name:"amjad" ,age:16} ,
+    {id:77 , name:"atefe" ,age:37} ,
+    {id:80 , name:"ehsan" ,age:34} ,
+    {id:88 , name:"sina"  ,age:26} ,
+    {id:90 , name:"pouria",age:21} ,
+    {id:99 , name:"kamran",age:33} ,
 ]
 
-export let userCount = users.length
+let userCount = users.length
 
-export function isLogin (userID) {
-    let isUserInUsers = users.some(user => user.id===userID)
-    return isUserInUsers
+function isLogin (userID){
+    let existResult = users.some(user => user.id == userID)
+    return existResult
+
 }
 
-export function userRegister (name,age) {
-    let newUserObject ={
-        id:Math.floor(Math.random()*100) ,
+let ides = []
+
+function usersIDs () {
+    users.forEach(user=> ides.push(user.id))
+
+}
+
+function userRegister (name="user",age=0){
+    
+    let newID = getNewID()
+
+    let newUser = {
+        id:newID ,
         name,
-        age,
+        age ,
     }
-    users.push(newUserObject) ;
-    return users
+
+    users.push(newUser);
+
+    return users 
 }
 
 
-// export { isLogin ,userRegister , userCount } 
+function getNewID () {
+    
+    usersIDs()
+    
+    let existIDInUsersOrNot = 1 ;
+    let newID = 0 ;
+    while(existIDInUsersOrNot !== -1 && existIDInUsersOrNot){
+        newID = Math.floor(Math.random()*100) ;
+        existIDInUsersOrNot = ides.findIndex(el=>el===newID)
+    }
+    return newID
+}
+
+
+
+
+userRegister("aaaa",111)
+userRegister("bbbb",222)
+userRegister("cccc",333)
+userRegister()
+console.log(users)
+
+
+
+export { isLogin, userRegister}
--- js/script.js
+++ js/script.js
@@ -1,23 +1,23 @@
 "use strict"
 
-import { isLogin , userRegister ,userCount as usersLength} from "./funcs.js"
+// import { isLogin , userRegister} from "./funcs.js"
 
-console.log(isLogin(19))
-;
-let newUserName = prompt("Enter User Name") ;
-let newUserAge = prompt("Enter User Age") ;
+// console.log(isLogin(19))
+// ;
+// let newUserName = prompt("Enter User Name") ;
+// let newUserAge = prompt("Enter User Age") ;
 
-console.log(userRegister(newUserName,newUserAge))
-
-
-console.log(usersLength)
+// console.log(userRegister(newUserName,newUserAge))
 
 
+// console.log(usersLength)
 
 
+/////////////////////////////////////////////////////
 
 
+import * as funcs from "./funcs.js"
 
 
-
-
+let newUser = funcs.userRegister("ali",3333)
+console.log(newUser)
