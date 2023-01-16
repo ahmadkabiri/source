ae1734503a64096bde658235c90a45b5a5ddfae2
AhmadKabiri <51478462+ahmadkabiri@users.noreply.github.com>
2023-01-16 12:45:39 +0330

    call-applay-bind

--- js/app.js
+++ js/app.js
@@ -1,38 +1,39 @@
 
+let user = {
+    id: 1, 
+    username : 'amin' ,
+    age:22 ,
+}
 
 
-// let user = {
-//     id : 1 ,
-//     name : "Ahmad" ,
-//     family : "Kabiri",
-//     walk (el) {
-//         this.name = el ;
-//     }
-// }
-
-
-
-// user.walk("reza")
-
-
-// console.log(user)
-
-
-function removeListItem (el) {
-    el.remove()
+let user2 = {
+    id: 2, 
+    username : 'ali' ,
+    age:19 ,
 }
 
 
 
 
 
+function showUser (score, city) {
+    console.log(this.username + " Is " + this.age + " Years Old :) => Score: ", score, "City " + city )
+}
+
+// showUser.call(user,20,"Tabriz")
+// showUser.call(user2,14,"Tehran")
 
 
+// showUser.apply(user,[20,"Tabriz"])
+// showUser.apply(user2,[14,"Tehran"])
 
 
+showUser.bind(user,22,"tabriz")()
+showUser.bind(user2,14,"tehran")()
 
+let bindingFunc = showUser.bind(user2,14,"tehran")
 
-
+bindingFunc()
 
 
 
