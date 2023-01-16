b2631517eb890706978bbdc961b1ca3f33125d08
AhmadKabiri <51478462+ahmadkabiri@users.noreply.github.com>
2023-01-16 17:48:08 +0330

    constructor function

--- js/app.js
+++ js/app.js
@@ -1,48 +1,35 @@
 
-let user1 = {
-    id: 1, 
-    username : 'amin' ,
-    age:22 ,
-    city: 'shiraz' ,
-    degree:'BSC'
+// function Animal () {
+//     this.name = 'dog'
+//     this.color = 'brown'
+//     this.canFly = false 
+//     this.footCount = 4
+
+// }
+
+// let dogAnimal = new Animal()
+
+
+// console.log(dogAnimal)
+
+
+function Person (id,name,age,job,type) {
+    this.id = id 
+    this.name = name
+    this.age = age
+    this.job = job
+    this.type = type
+
 }
 
 
-let user2 = {
-    id: 2, 
-    username : 'ali' ,
-    age:19 ,
-    city: "Qazvin" ,
-    degree:"MSC"
-}
+let userAli = new Person(1,'ali',19,"Digital Marketing",'user')
 
+let userAmin = new Person(2,'amin',25,'Js developer','Admin')
 
+console.log(userAli)
 
-
-
-function showUser (score, lastName) {
-    console.log(`Your id is ${this.id} and your name is ${this.username},your age is ${this.age}, You live in ${this.city} . Your degree is ${user1.degree} , you gained ${score}, your last name is ${lastName}`)
-}
-
-// showUser.call(user1,20,"Tabriz")
-// showUser.call(user2,14,"Tehran")
-
-showUser.call(user1,95,"samak")    // khod be khod tabe ejra shod !!
-showUser.call(user2,98,"kabiri") // khod be khod tabe ejra shod !!
-
-showUser.apply(user1,[87,"Tabriz"]) // khod be khod tabe ejra shod !!
-showUser.apply(user2,[56,"Tehran"]) // khod be khod tabe ejra shod !!
-
-
-let bindingFunc1 =  showUser.bind(user1,22,"tabriz") // ma bayad farakhanish konim!
-let bindingFunc2 =  showUser.bind(user2,14,"tehran") // ma bayad farakhanish konim!
-
-bindingFunc1()
-bindingFunc2()
-
-
-
-
+console.log(userAmin)
 
 
 
