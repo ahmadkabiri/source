8983fd6baa29ee896587928afe2c7d534f0cd13e
AhmadKabiri <51478462+ahmadkabiri@users.noreply.github.com>
2023-01-17 21:28:28 +0330

    map

--- js/app.js
+++ js/app.js
@@ -1,33 +1,57 @@
 
-// let userName = 'Mohammad Amin' ; // iterable
+
+// // for (let userProb of user) {
+    // //     // Codes ...
+    // // }
+    
+    // // console.log(user)
+    
+    
+    // let  mapUser = new Map() 
+    
+    // mapUser.set("id",1)
+    // mapUser.set("firstName","Qadir")
+    // mapUser.set("lastName","Yolme")
+    // mapUser.set("age",20)
+    
+    // mapUser.delete("age")
+    
+    // console.log(mapUser.get("id"))
+    
+    // console.log(mapUser)
+    // console.log(mapUser.size)
+    // console.log(mapUser.has('firstName'))
+    
+    
+    // mapUser.forEach((value ,key)=>{
+        //     console.log(key+ "=>" + value )
+        // })
+        
+        
+        
+        
+        
+        let user = {
+            id : 1 ,
+            firstName : "Qadir" ,
+            lastName : "Yolme" ,
+            age : 20 
+        }
+
+
+let userMap = new Map (Object.entries(user))
+
+
+let userObj = Object.fromEntries(userMap)
 
 
 
-// let setUserName = new Set(userName)
-
-// console.log(setUserName)
-
-
-let items = [
-    {id:1} ,
-    {id:2} ,
-    {id:3} ,
-]
-
-let setItemsData = new Set(items)
-
-setItemsData.add(22)
-
-
-console.log(
-setItemsData.has(setItemsData[0])
-)
-
-console.log(setItemsData)
 
 
 
+console.log(user)
 
+console.log(userMap)
 
 
 
