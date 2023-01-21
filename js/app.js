a7f41bec202d264642b222fa4e70c796b5b2d36e
AhmadKabiri <51478462+ahmadkabiri@users.noreply.github.com>
2023-01-21 12:01:25 +0330

    promise-cdn-error-load

--- js/app.js
+++ js/app.js
@@ -1,98 +1,37 @@
-// sabzlearn code .............
 
-// let fetchData = () => {
-//     return new Promise((resolve, reject) => {
-//         setTimeout(() => {
-//             console.log("Fetching Data Completed!") 
-//             resolve({id:1 , message:"Fetching Data Completed"})
-//         },2000);
-//     })
-// }
-
-
-// const parseData = (data) =>{
-//     return new Promise((resolve, reject) => {
-//         let parsedOutput = `Parsed the data for id: ${data.id} and with message : ${data.message}`
-//         setTimeout(() => {
-            
-//             console.log("Fake parse")
-//             resolve({parsed:parsedOutput})
-//         }, 2000);
-//     })
-// }
-
-
-
-// const showData = (response)=>{
-//     return new Promise((resolve, reject) => {
-//         setTimeout(() => {
-//             let fakeData = dataBaseData
-//             console.log(response.parsed)
-//             console.log(response)
-//             resolve(resolve)
-//         }, 2000);
-//     })
-// }
-
-
-// fetchData()
-// .then(parseData)
-// .then(showData)
-
-/////////////////////////////////////////////////////////////
-
-// my code.......
-
-
-function fetchData () {
+const loadExtenalFile = cdn => {
     return new Promise((resolve, reject) => {
-        setTimeout(() => {
-            console.table([
-                {id:1 , name:'Ali' , age : 15},
-                {id:2 , name:'Reza' , age : 27},
-                {id:3 , name:'Amir' , age : 25},
-                {id:4 , name:'Shahin' , age : 29},
-            ])
-            resolve([
-                {id:1 , name:'Ali' , age : 15},
-                {id:2 , name:'Reza' , age : 27},
-                {id:3 , name:'Amir' , age : 25},
-                {id:4 , name:'Shahin' , age : 29},
-            ])
-        }, 1000);
+        const linkTag =  document.createElement("link") ;
+
+        linkTag.rel = "stylesheet"
+        linkTag.href = cdn 
+
+        linkTag.onload = () =>resolve("File Loaded SuccessFully")
+        linkTag.onerror = () =>reject(new Error("File Not Loaded"))
+
+        document.head.append(linkTag)
+
     })
 }
 
-function makePhrases (arr) {
-    return new Promise((resolve, reject) => {
-        setTimeout(() => {
-            let newIDs = []
-            let newP = '' ;
-            arr.forEach(user=>{
-                 newP = ` ${user.name}'s id is : ${user.id} and he is ${user.age} years old` ;
-                 newIDs.push(newP)
-                 newP = '' ;
-            })
-            console.table(newIDs)
-            resolve(newIDs);
-        }, 1000);
-    })
-}
 
-function showPhrases (phrase) {
-    return new Promise((resolve, reject) => {
-        setTimeout(() => {
-            phrase.forEach(el=>{
-                console.log(el);
-                resolve()
-            })
-        }, 1000);
-    })
-}
+loadExtenalFile('css/style.css')
+.then((response)=>{
+    console.log(response)
+    document.body.insertAdjacentHTML('afterbegin',`<h1>${response}</h1>`)
+})
+.catch(err => {
+    console.log(err);
+})
 
-fetchData()
-.then(makePhrases)
-.then(showPhrases)
+
+
+
+
+
+
+
+
 
 
 
