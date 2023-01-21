da7068fa14b89645e75b2dd102d66c02a1786326
AhmadKabiri <51478462+ahmadkabiri@users.noreply.github.com>
2023-01-21 11:40:38 +0330

    promise - chaining handler- three function

--- js/app.js
+++ js/app.js
@@ -1,62 +1,98 @@
+// sabzlearn code .............
+
+// let fetchData = () => {
+//     return new Promise((resolve, reject) => {
+//         setTimeout(() => {
+//             console.log("Fetching Data Completed!") 
+//             resolve({id:1 , message:"Fetching Data Completed"})
+//         },2000);
+//     })
+// }
 
 
-let wordPromise = new Promise((resolve, reject) => {
-    let text = "SabzLearn"
-
-    if (text) {
-        resolve(text)
-    }else{
-        reject(new Error("Text is empty"))
-    }
-
-})
-
-
-wordPromise
-.then( response => response.split(""))
-.then( response2 => response2.reverse())
-.then( response3 => response3.join(""))
-.then( response4 => console.log(response4))
-.catch( err => console.log(err))
-
-// wordPromise
-// .then((response)=>{
-//     console.log(response)
-//     return response.split("")
-// })
-// .then((response2)=>{
-//     console.log(response2)
-//     return response2.reverse()
-// })
-// .then((response3)=>{
-//     console.log(response3)
-//     return response3.join("")
-// })
-// .then((response4)=>{
-//     console.log(response4)
-// })
-// .catch((err)=>{
-//     console.log(err)
-// })
+// const parseData = (data) =>{
+//     return new Promise((resolve, reject) => {
+//         let parsedOutput = `Parsed the data for id: ${data.id} and with message : ${data.message}`
+//         setTimeout(() => {
+            
+//             console.log("Fake parse")
+//             resolve({parsed:parsedOutput})
+//         }, 2000);
+//     })
+// }
 
 
 
+// const showData = (response)=>{
+//     return new Promise((resolve, reject) => {
+//         setTimeout(() => {
+//             let fakeData = dataBaseData
+//             console.log(response.parsed)
+//             console.log(response)
+//             resolve(resolve)
+//         }, 2000);
+//     })
+// }
 
 
+// fetchData()
+// .then(parseData)
+// .then(showData)
+
+/////////////////////////////////////////////////////////////
+
+// my code.......
 
 
+function fetchData () {
+    return new Promise((resolve, reject) => {
+        setTimeout(() => {
+            console.table([
+                {id:1 , name:'Ali' , age : 15},
+                {id:2 , name:'Reza' , age : 27},
+                {id:3 , name:'Amir' , age : 25},
+                {id:4 , name:'Shahin' , age : 29},
+            ])
+            resolve([
+                {id:1 , name:'Ali' , age : 15},
+                {id:2 , name:'Reza' , age : 27},
+                {id:3 , name:'Amir' , age : 25},
+                {id:4 , name:'Shahin' , age : 29},
+            ])
+        }, 1000);
+    })
+}
 
+function makePhrases (arr) {
+    return new Promise((resolve, reject) => {
+        setTimeout(() => {
+            let newIDs = []
+            let newP = '' ;
+            arr.forEach(user=>{
+                 newP = ` ${user.name}'s id is : ${user.id} and he is ${user.age} years old` ;
+                 newIDs.push(newP)
+                 newP = '' ;
+            })
+            console.table(newIDs)
+            resolve(newIDs);
+        }, 1000);
+    })
+}
 
+function showPhrases (phrase) {
+    return new Promise((resolve, reject) => {
+        setTimeout(() => {
+            phrase.forEach(el=>{
+                console.log(el);
+                resolve()
+            })
+        }, 1000);
+    })
+}
 
-
-
-
-
-
-
-
-
-
+fetchData()
+.then(makePhrases)
+.then(showPhrases)
 
 
 
