0443983304c6217b3ff6201a222e32429f6a921a
AhmadKabiri <51478462+ahmadkabiri@users.noreply.github.com>
2023-01-21 10:29:22 +0330

    chaining handlers

--- js/app.js
+++ js/app.js
@@ -1,120 +1,63 @@
-import * as datas from "./script.js"
-
-// const inputElem = document.querySelector(".input")
-
-// let bookContainerElem = document.querySelector(".book-container")
-
-// let promisedList ='' ;
-// let list = '' ;
-// inputElem.addEventListener("keyup",(event)=>{
-//     list = '' ;
-//     bookContainerElem.innerHTML = '' ;
-//     let existBooks = []
-
-//     if(inputElem.value){
-//         let searchedWords = event.target.value
-//          existBooks = datas.books.filter(book=>{
-//             return book.name.includes(searchedWords)
-//         })
-//     }
 
 
-//     new Promise((resolve,reject)=>{
-//         setTimeout(()=>{
-//             existBooks.forEach(item=>{
-//                 list += `<div class="book">
-//                 <p>${item.name}</p>
-//               </div>`
-//             })
-//             if(list){
-//                 resolve()
-//             }else{
-//                 reject()
-//             }
-//         },3000)
+let wordPromise = new Promise((resolve, reject) => {
+    let text = "SabzLearn"
 
-//     }).then(()=>{
-//         bookContainerElem.insertAdjacentHTML("beforeend",list)
-//     })
-//     .catch(()=>{
-//         console.log("chizi nist")
-//     })
+    if (text) {
+        resolve(text)
+    }else{
+        reject(new Error("Text is empty"))
+    }
 
-//     bookContainerElem.insertAdjacentHTML("beforeend",list)
+})
 
 
+wordPromise
+.then( response => response.split(""))
+.then( response2 => response2.reverse())
+.then( response3 => response3.join(""))
+.then( response4 => console.log(response4))
+.catch( err => console.log(err))
+
+// wordPromise
+// .then((response)=>{
+//     console.log(response)
+//     return response.split("")
+// })
+// .then((response2)=>{
+//     console.log(response2)
+//     return response2.reverse()
+// })
+// .then((response3)=>{
+//     console.log(response3)
+//     return response3.join("")
+// })
+// .then((response4)=>{
+//     console.log(response4)
+// })
+// .catch((err)=>{
+//     console.log(err)
 // })
 
 
-// console.log(datas.books)
-
-
-/////////////////////////////////////////////////
-// function addBook (name, price) {
-//     let newBook = {
-//         id : books.length + 1 ,
-//         name,
-//         price,
-//     }
-    
-//     return new Promise((resolve,reject) => {
-//             setTimeout(()=>{
-//             books.push(newBook)
-//             if(books.push(newBook)){
-//                 resolve()
-//             }else{
-//                 reject()
-//             }
-//         },2000)
-//         })
-
-// }
-
-// addBook('golestan',90_000).then(()=>{
-//     console.log(books)
-// })
-
-
-// console.log(addBook())
-
-
-// Promise = 'قول دادن ' ES6 :)) ()
 
 
 
-let myPromise = new Promise((Resolved,Rejected) => {  // Reso.. and Reje.. both are callback functions
-    const loginFlag = !true ;
-    Rejected()
-    Resolved()
-    setTimeout(()=>{
-        console.log("Login Check !")
-        // if(loginFlag){
-        //     Resolved( "شما می‌توانید وارد پنل کاربری خود شوید")
-        // }else{
-        //     Rejected(new Error("شما لاگین نکرده‌اید")) // این خیلی مهمه ها که کجا بیای new Error استفاده بکنی
-        // }
-    },3000)
-})
 
-// function success () {
-//     console.log("Success :)")
-// }
 
-// function error () {
-//     console.log("Error :(")
 
-// }
 
-// myPromise.then(success,error)
 
-myPromise
-.then((success)=>{
-    console.log(success)
-}) // then is a handler
-.catch((err)=>{
-    console.log(err)
-})  // catch is a handler
-.finally(() => {
-    console.log("پرامیس انجام شد")
-})
-console.log(myPromise)
+
+
+
+
+
+
+
+
+
+
+
+
+
