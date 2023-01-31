35bae50e80315be9346259332415471eac01320f
AhmadKabiri <51478462+ahmadkabiri@users.noreply.github.com>
2023-01-31 20:28:58 +0330

    class-methods-properties

--- js/app.js
+++ js/app.js
@@ -1,28 +1,71 @@
 
 
 
-// window.addEventListener("load",()=>{
-    
-//     fetch('https://jsonplaceholder.typicode.com/posts')
+// function Person (firstname,lastname,age) {
+//     // this.firstname = firstname 
+//     // this.lastname = lastname
+//     // this.age = age
 
-//     .then(res => res.json())
-//     .then(data => {
-//         data.forEach(post => {
-//             console.log(post)
-//         });
-//     })
+//     this.getFirstName = function (){
+//         return this.firstname
+//     }
+//     this.getLastName = function (){
+//         return this.lastname
+//     } 
+//     this.getAge = function (){
+//         return this.age
+//     }
+//     this.setFirstName = function (newName){
+//       this.firstname = newName
+//     }
+//     this.setLastName = function (newName){
+//       this.lastname = newName
+//     } 
+//     this.setAge = function (newAge){
+//         this.age = newAge
+//     }
 
-// })
+// }
 
-//
+// // let aliPerson = new Person("ali","kabiri",18)
+// let aliPerson = new Person()
+
+// // aliPerson.setAge(22)
+
+// aliPerson.setFirstName("AHMAD")
+// aliPerson.setLastName("Kabiri")
+// aliPerson.setAge(19)
+
+// console.log(aliPerson.getFirstName("AHMAD") )
+// console.log(aliPerson.getLastName("Kabiri") )
+// console.log(aliPerson.getAge(19) )
 
 
-let url = 'https://jsonplaceholder.typicode.com/posts/'
-
-let postID = 2
+// console.log(aliPerson)
 
 
 
-fetch(`${url}${postID}`)
-.then(res => res.json())
-.then(mainPost => console.log(mainPost))
+class Person1 {
+    constructor(personName,personLastName,Age){
+        this.username = personName ;
+        this.personLastName = personLastName ;
+        this.age = Age
+    }
+
+    startDev () {
+        console.log(`برنامه نویس ${this.username} کارشو استارت زد`);
+    }
+
+}
+
+
+let personAli = new Person1("Ali","hosseini",22)
+
+console.log(personAli);
+
+personAli.startDev()
+
+
+
+
+
