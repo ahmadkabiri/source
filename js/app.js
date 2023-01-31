7fc481c9ca74ec115571fd8b88a966169ff7b99b
AhmadKabiri <51478462+ahmadkabiri@users.noreply.github.com>
2023-01-31 15:12:24 +0330

    post-get-delete-put method in fire base

--- js/app.js
+++ js/app.js
@@ -18,7 +18,7 @@ form.addEventListener("submit",(event)=>{
     }
 
     // POST REQUEST 
-    fetch('https://backend-project-7e570-default-rtdb.firebaseio.com/false.json',{
+    fetch('https://backend-project-7e570-default-rtdb.firebaseio.com/users.json',{
         method : 'POST' ,
         headers : {
             'Content-type' : 'application/json'
@@ -44,7 +44,79 @@ function clearData () {
 }
 
 
+//////////
+///////// marboot be GET
+//////////
+
+let userData = null ;
 
 
 
+
+
+
+
+window.addEventListener("load",()=>{
+    fetch("https://backend-project-7e570-default-rtdb.firebaseio.com/users.json")
+    .then(res=>res.json())
+    .then(data => {
+        userData = Object.entries(data) 
+        userData.forEach(el=>{
+            console.log(el)
+
+        })
+    }) // ba object.entries mitunim rooye object loop bezanim
+    
+})
+
+
+///////////
+/////////// MRBOOT BE DELETE
+///////////
  
+
+
+
+// setTimeout(() => {
+    
+//     fetch(`https://backend-project-7e570-default-rtdb.firebaseio.com/users/${userData[0][0]}.json`,{
+//         method : 'DELETE'
+//     }).then(res => console.log(res))
+
+// }, 3000);
+
+
+
+const userNewData = {
+    firstname : "Ahmad",
+    lastname : "Kabiri",
+    password : "1a2b3c4d"
+}
+
+setTimeout(() => {
+
+fetch(`https://backend-project-7e570-default-rtdb.firebaseio.com/users/${userData[0][0]}.json`,{
+    method : "PUT" ,
+    headers : {
+        "Content-type" : "application/json"
+    },
+    body : JSON.stringify(userNewData)
+})
+
+}, 3000);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
