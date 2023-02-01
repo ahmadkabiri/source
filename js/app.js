755e400385f4a4eb740b1ff43aaa4ef7964eaa24
AhmadKabiri <51478462+ahmadkabiri@users.noreply.github.com>
2023-02-02 01:12:15 +0330

    class - inheritance-extend-super

--- js/app.js
+++ js/app.js
@@ -1,69 +1,64 @@
 
 
+class Person {
 
-// function Person (firstname,lastname,age) {
-//     // this.firstname = firstname 
-//     // this.lastname = lastname
-//     // this.age = age
+    constructor(personname,personAge,personJob,personAddress){
 
-//     this.getFirstName = function (){
-//         return this.firstname
-//     }
-//     this.getLastName = function (){
-//         return this.lastname
-//     } 
-//     this.getAge = function (){
-//         return this.age
-//     }
-//     this.setFirstName = function (newName){
-//       this.firstname = newName
-//     }
-//     this.setLastName = function (newName){
-//       this.lastname = newName
-//     } 
-//     this.setAge = function (newAge){
-//         this.age = newAge
-//     }
-
-// }
-
-// // let aliPerson = new Person("ali","kabiri",18)
-// let aliPerson = new Person()
-
-// // aliPerson.setAge(22)
-
-// aliPerson.setFirstName("AHMAD")
-// aliPerson.setLastName("Kabiri")
-// aliPerson.setAge(19)
-
-// console.log(aliPerson.getFirstName("AHMAD") )
-// console.log(aliPerson.getLastName("Kabiri") )
-// console.log(aliPerson.getAge(19) )
-
-
-// console.log(aliPerson)
-
-
-
-class Person1 {
-    constructor(personName,personLastName,Age){
-        this.username = personName ;
-        this.personLastName = personLastName ;
-        this.age = Age
+        this.personname = personname ;
+        this.personAge = personAge ;
+        this.personJob = personJob ;
+        this.personAddress = personAddress ;
     }
+
 
     startDev () {
-        console.log(`برنامه نویس ${this.username} کارشو استارت زد`);
+        console.log(` برنامه نویس کارشو استارت زد${this.personname}`)
     }
 
+    getAge() {
+
+        return this.personAge
+    }
 }
 
 
-let personAli = new Person1("Ali","hosseini",22)
+class Prefessor extends Person {
 
-console.log(personAli);
+    constructor (personname,personAge,personJob,personAddress,personLesson,workExperiance) {
 
-personAli.startDev()
+        // this.personname = personname ;
+        // this.personAge = personAge ;
+        // this.personJob = personJob ;
+        // this.personAddress = personAddress ;
+
+        super(personname,personAge,personJob,personAddress)
+
+        this.personLesson = personLesson ;
+        this.workExperiance = workExperiance ;
+
+    }
+
+    startDev () {
+        console.log(super.getAge())
+        console.log(`استاد تدریسو شروع کرد${this.personname}`)
+    }
+
+
+
+}
+
+let perfessorRamin = new Prefessor('Ramin',32,'Prefessor','Tabriz','C++',8)
+
+// console.log(perfessorRamin.getAge())
+console.log(perfessorRamin.startDev())
+
+
+
+
+
+
+
+
 
 
 
