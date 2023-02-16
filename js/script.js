457383f3163cb0b92d69e0b35ee97e67aca0a33e
AhmadKabiri <51478462+ahmadkabiri@users.noreply.github.com>
2023-02-16 18:19:48 +0330

    cookie with login page

--- js/script.js
+++ js/script.js
@@ -1,17 +1,80 @@
-﻿// Custom Element  المنت های سفارشی سازی شده 
+﻿const $ = document ;
 
-// Component 
+const usernameInput = $.querySelector("#username")
 
-// DRY => Don't Repeat Yourself 
+const passwordInput = $.querySelector("#password") 
 
-// function () {}
+const rememberMeCheckbox = $.querySelector(".ck")
 
-
-// let pElem = document.querySelector("p")
-
-// console.log(pElem)
+const loginBtn = $.querySelector("button")
 
 
-import { SiteUser } from "../components/site-user/site-user.js";
 
-window.customElements.define("site-user",SiteUser)
+
+
+
+
+
+function setCookie (cookieName,cookieValue,exDay) {
+    const now = new Date() ;
+
+    let expireDay = now.setTime(now.getTime() + exDay*24*60*60*1000)
+
+    document.cookie = `${cookieName}=${cookieValue};path=/;expires=${now}`
+
+}
+
+
+
+function clearInput () {
+    usernameInput.value = '' ;
+    passwordInput.value = '' ;
+}
+
+
+
+
+loginBtn.addEventListener("click",(event)=>{
+    event.preventDefault()
+    
+    if (rememberMeCheckbox.checked) {
+        setCookie("login-token",usernameInput.value,10)
+    }
+
+    clearInput()
+})
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
