40a621bf8a74a2a4530c6e11f41443519fa76bf8
AhmadKabiri <51478462+ahmadkabiri@users.noreply.github.com>
2023-02-16 19:52:56 +0330

    promise hell 2

--- js/script.js
+++ js/script.js
@@ -1,34 +1,42 @@
 ﻿
-const userLogin = (username,password,callback)=>{
-    setTimeout(() => {
-        callback({
-            username,
-            password,
-            email : "amin@gmail.com"
+const userLogin = (username,password)=>{
+    return new Promise((resolve,reject)=>{
+        setTimeout(() => {
+            resolve({
+                username,
+                password,
+                email : "amin@gmail.com"
+                })
+        },4000);
+    })
+}
+
+const userCourses = (username) => {
+    return new Promise((resolve,reject)=>{
+        setTimeout(() => {
+            resolve([
+                {id:21 , title:"js export" , price:"free"} ,
+                {id:34 , title:"redux" , price:"free"} ,
+    
+            ])
+        },3000)
+
+    })
+}
+
+const mainVideoInfo = (courseTitle)=>{
+    return new Promise((resolve,reject)=>{
+        setTimeout(()=>{
+            resolve({
+                id:34 ,
+                title:"redux expert" ,
+                price : "free" ,
+                teacher : "saeedi" ,
+                student:2091 
             })
-    },4000);
-}
+        },3000)
 
-const userCourses = (username,callback) => {
-    setTimeout(() => {
-        callback([
-            {id:21 , title:"js export" , price:"free"} ,
-            {id:34 , title:"redux" , price:"free"} ,
-
-        ])
-    },3000)
-}
-
-const mainVideoInfo = (courseTitle,callback)=>{
-    setTimeout(()=>{
-        callback({
-            id:34 ,
-            title:"redux expert" ,
-            price : "free" ,
-            teacher : "saeedi" ,
-            student:2091 
-        })
-    },3000)
+    })
 }
 
 
@@ -37,16 +45,33 @@ console.log("سایت برای کاربر لود شد")
 
 
 
-const loginInfos = userLogin("amin_saeedi","0101",(userObject)=>{
+// const loginInfos = userLogin("amin_saeedi","0101",(userObject)=>{
 
-    console.log("کاربر لاگین شد" , userObject )
-    userCourses(userObject.username , (userAllCourses)=>{
-        console.log("user COURSES: " , userAllCourses)
-        mainVideoInfo(userAllCourses[1].title,(mainInfos)=>{
-            console.log("Main course Infos :",mainInfos)
-        })
-    })
-}) 
+//     console.log("کاربر لاگین شد" , userObject )
+//     userCourses(userObject.username , (userAllCourses)=>{
+//         console.log("user COURSES: " , userAllCourses)
+//         mainVideoInfo(userAllCourses[1].title,(mainInfos)=>{
+//             console.log("Main course Infos :",mainInfos)
+//         })
+//     })
+// }) 
+
+
+userLogin()
+    .then(userObject=>userCourses(userObject.username))
+    .then(userAllCourses => mainVideoInfo(userAllCourses[1].title))
+    .then(mainInfos => console.log(mainInfos))
+
+
+
+
+
+
+
+
+
+
+
 
 
 
